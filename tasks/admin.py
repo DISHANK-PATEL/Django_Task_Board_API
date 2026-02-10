a2c873448fdb2ee62ad77f49dcfd82c073c94a80
DYNAMIC_DISHANK <dishankpatel16082003@gmail.com>
2026-02-10 10:43:06 +0530

    Added the Task Creation, User Management, Activity Logs to admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Task, ActivityLog
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+ 
+    list_display = ('id', 'title', 'status', 'creator', 'assigned_to', 'due_date')
+
+    list_filter = ('status', 'due_date', 'creator', 'assigned_to')
+   
+    search_fields = ('title', 'description', 'creator__email', 'assigned_to__email')
+    
+    fieldsets = (
+        ('Task Info', {
+            'fields': ('title', 'description', 'status', 'due_date')
+        }),
+        ('Assignments', {
+            'fields': ('creator', 'assigned_to')
+        }),
+    )
+
+@admin.register(ActivityLog)
+class ActivityLogAdmin(admin.ModelAdmin):
+    
+    list_display = ('timestamp', 'actor', 'action', 'get_task_link', 'details')
+
+    list_filter = ('action', 'timestamp', 'actor')
+
+    search_fields = ('details', 'actor__email', 'task__title')
+
+    readonly_fields = ('timestamp', 'actor', 'action', 'task', 'details')
+
+    def get_task_link(self, obj):
+        if obj.task:
+            return obj.task.title
+        return "Deleted Task"
+    get_task_link.short_description = "Related Task"
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
+from django.contrib import admin 
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    ordering = ('email',)
+    
+    list_display = ('email', 'is_staff', 'is_active') 
+    
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        
+        ('Important dates', {'fields': ('last_login',)}), 
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password'),
+        }),
+    )
